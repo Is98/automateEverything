19551efc807e3c69f22bbae6b47d62703e854adf
is98 <is98@uni.brighton.ac.uk>
2015-12-17 16:41:17 +0000

    user friendliness

--- agent.sh
+++ agent.sh
@@ -1,2 +1,12 @@
+####################################################################
+#                                                                  #
+#                change this to new hostname and IP                #
+#                                                                  #
+####################################################################
+#                                                                  #
 echo "192.168.1.150 hmaster.netbuilder.private puppet" >> /etc/hosts
-curl -k https://hmaster.netbuilder.private:8140/packages/current/install.bash | sudo bash
+#                                                                  #
+####################################################################
+
+
+curl -k https://puppet:8140/packages/current/install.bash | sudo bash
--- enterprise_install.sh
+++ enterprise_install.sh
@@ -1,6 +1,16 @@
 sudo su
-echo "127.0.0.1 hmaster.netbuilder.private puppet" >> /etc/hosts
+
+####################################################################
+#                                                                  #
+#                change this to new hostname and IP                #
+#                                                                  #
+####################################################################
+#                                                                  #
+echo "127.0.0.1 hmaster.netbuilder.private puppet"     >> /etc/hosts
 echo "192.168.1.150 hmaster.netbuilder.private puppet" >> /etc/hosts
+#                                                                  #
+####################################################################
+
 cd /vagrant
 wget http://192.168.1.6:8080/downloads/puppet-enterprise-2015.2.0-ubuntu-14.04-amd64.tar.gz | grep "puppet-enterprise"
 tar zxf puppet-enterprise-2015.2.0-ubuntu-14.04-amd64.tar.gz | grep "puppet-enterprise"
